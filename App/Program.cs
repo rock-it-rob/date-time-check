1fa83f3ee5d175013f6bb4b47f5da744dce069d2
Rob Benton <rob.benton7@gmail.com>
2024-01-09 10:18:19 -0600

    missed files

---
+++ App/Program.cs
@@ -0,0 +1,48 @@
+﻿using App.Services;
+using DateTimeCheck.Data.Connectors;
+using DateTimeCheck.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+
+var hostBuilder = Host.CreateApplicationBuilder();
+
+hostBuilder
+    .Services
+    .AddScoped<IThingService, ThingService>()
+    .AddDbContext<DataDbContext>(builder =>
+    {
+        var config = hostBuilder.Configuration.GetSection("DataSource");
+        var connectionStringBuilder = new NpgsqlConnectionStringBuilder();
+        config.Bind(connectionStringBuilder);
+        builder.ConnectWithBuilder(connectionStringBuilder);
+    });
+
+using var host = hostBuilder.Build();
+
+host.Start();
+var service = host.Services.GetRequiredService<IThingService>();
+
+var writer = new StringWriter();
+
+// Insert
+var thing = await service.CreateAsync();
+writer.WriteLine($"Insert Thing: {thing}");
+
+// Update
+thing.When = DateTime.UtcNow;
+writer.WriteLine($"Update Thing: {thing}");
+await service.UpdateAsync(thing);
+
+// Re-query
+var verify = await service.GetById(thing.Id);
+writer.WriteLine($"Verify Thing: {verify}");
+
+writer.Flush();
+Console.Write(writer.ToString());
+
+for (var i = 0; i < 4; ++i)
+{
+    Console.WriteLine(DateTime.UtcNow.ToString("O"));
+}
---
+++ Data/Migrations/20240109153421_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DateTimeCheck.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "things",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    when = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_things", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "things");
+        }
+    }
+}
